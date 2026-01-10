b56b5d3d2b2fa2e5c48a0194b0ea6690de08a859
MandellaCotologFun <166307003+MandellaCotologFun@users.noreply.github.com>
2026-01-10 18:43:12 +0300

    Add files via upload

---
+++ hakaton/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+facts_list = [
+    "Белые медведи теряют лёд.",
+    "Глобальное потепление разрушает их среду обитания.",
+    "Медведям приходится далеко плыть."
+]
+
+memes_list = [
+    "memes/bear1.png",
+    "memes/meme2.png",
+    "memes/meme3.png"
+]
+
+@app.route("/")
+def home():
+    return render_template(
+        "index.html",
+        fact=random.choice(facts_list),
+        meme=random.choice(memes_list)
+    )
+
+app.run(debug=True)
